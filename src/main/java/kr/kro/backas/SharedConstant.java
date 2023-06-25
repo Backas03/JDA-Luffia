9e3677395c6a16eda040e78f6d791d8d82ce25f6
wkgus0618 <wkgus0618@naver.com>
2023-06-25 14:01:41 +0900

    Add OP Guild Id

--- src/main/java/kr/kro/backas/SharedConstant.java
+++ src/main/java/kr/kro/backas/SharedConstant.java
@@ -3,6 +3,7 @@ package kr.kro.backas;
 public final class SharedConstant {
     public static final long MAIN_GUILD_ID = 791974345965961237L;
     public static final long DEV_GUILD_ID = 1023103013092610111L;
+    public static final long OP_GUILD_ID = 1121632283154202694L;
     public static final long PUBLISHED_GUILD_ID = MAIN_GUILD_ID;
 
     public static final String DEBUG_VERSION = "Luffia/Debug.1.0.0-Debug";
