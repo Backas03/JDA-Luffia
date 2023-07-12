b9f0b61314759884574de53178b360dc26ac992d
wkgus0618 <wkgus0618@naver.com>
2023-07-12 23:58:07 +0900

    Remove debug version

--- src/main/java/kr/kro/backas/SharedConstant.java
+++ src/main/java/kr/kro/backas/SharedConstant.java
@@ -6,8 +6,7 @@ public final class SharedConstant {
     public static final long OP_GUILD_ID = 1121632283154202694L;
     public static final long PUBLISHED_GUILD_ID = MAIN_GUILD_ID;
 
-    public static final String DEBUG_VERSION = "Luffia/1.1.3-Debug";
-    public static final String RELEASE_VERSION = "Luffia/1.1.3.unstable-Release";
+    public static final String RELEASE_VERSION = "Luffia/1.1.4.unstable-Release";
 
     public static final String LICENSE = "GPL-3.0 license";
 
