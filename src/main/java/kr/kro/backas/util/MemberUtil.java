a0c8324956814d19876dfccdcef9ae3a0fd6caeb
wkgus <wkgus0618@naver.com>
2023-07-13 00:02:09 +0900

    Merge methods into MemberUtil

--- src/main/java/kr/kro/backas/util/MemberUtil.java
+++ src/main/java/kr/kro/backas/util/MemberUtil.java
@@ -3,6 +3,7 @@ package kr.kro.backas.util;
 import kr.kro.backas.Main;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.User;
 
 public final class MemberUtil {
     public static Member getMember(long userId) {
@@ -10,6 +11,16 @@ public final class MemberUtil {
         Member member = guild.getMemberById(userId);
         if (member != null) return member;
         return guild.retrieveMemberById(userId).complete();
+    }
+
+    public static String getName(Member member) {
+        return getName(member.getUser());
+    }
+
+    public static String getName(User user) {
+        return user.getGlobalName() != null ?
+                user.getGlobalName() :
+                String.format("%#s", user);
     }
 
 }
