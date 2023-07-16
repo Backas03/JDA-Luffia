e7af486db8bc495f5b37b2c7a10af8641f1befd9
wkgus0618 <wkgus0618@naver.com>
2023-07-16 19:45:01 +0900

    필요없는 클래스 제거

--- src/main/java/kr/kro/backas/music/service/MusicService.java
+++
@@ -1,9 +0,0 @@
-package kr.kro.backas.music.service;
-import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
-
-public interface MusicService {
-
-    String getIdentifier();
-
-    void loadItem(String query, AudioPlayerManager manager);
-}
