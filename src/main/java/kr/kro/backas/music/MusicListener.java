bba824a845424980c18465626002e327050eae33
wkgus0618 <wkgus0618@naver.com>
2023-07-16 19:47:30 +0900

    음악 재생중 해당 음성채팅방에 아무도 없으면 퇴장처리

--- src/main/java/kr/kro/backas/music/MusicListener.java
+++ src/main/java/kr/kro/backas/music/MusicListener.java
@@ -2,20 +2,27 @@ package kr.kro.backas.music;
 
 import kr.kro.backas.Main;
 import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.channel.unions.AudioChannelUnion;
+import net.dv8tion.jda.api.entities.channel.middleman.AudioChannel;
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceUpdateEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MusicListener extends ListenerAdapter {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(MusicListener.class);
+
     @Override
     public void onGuildVoiceUpdate(@NotNull GuildVoiceUpdateEvent event) {
-        /*
-        AudioChannelUnion channelLeft = event.getChannelLeft();
+        AudioChannel channelLeft = event.getOldValue();
         if (channelLeft == null) {
+            return;
+        }
+        if (event.getOldValue() == null) {
             return;
         }
         if (event.getMember().getUser().isBot()) {
@@ -23,17 +30,17 @@ public class MusicListener extends ListenerAdapter {
                     .getMusicPlayerManager()
                     .getTrackScheduler()
                     .onQuit();
+            LOGGER.info("track data cleared because the bot has left the voice channel. {voiceChannelId={}}", channelLeft.getIdLong());
             return;
         }
-        List<Member> members = channelLeft.getMembers();
+        List<Member> members = new ArrayList<>(channelLeft.getMembers());
         members.removeIf(member -> member != null && member.getUser().isBot());
         if (members.isEmpty()) {
             Main.getLuffia()
                     .getMusicPlayerManager()
                     .getTrackScheduler()
                     .quit();
+            LOGGER.info("음악 재생중 청자가 모두 퇴장하여 연결을 끊습니다. {voiceChannelId={}}", channelLeft.getIdLong());
         }
-
-         */
     }
 }
