3b258d4c985a824f08f00ec7364f1b6431a3e525
wkgus0618 <wkgus0618@naver.com>
2023-07-17 15:47:39 +0900

    버전 업 Luffia/1.1.8.unstable-Release

--- src/main/java/kr/kro/backas/SharedConstant.java
+++ src/main/java/kr/kro/backas/SharedConstant.java
@@ -7,7 +7,7 @@ public final class SharedConstant {
 
     public static final boolean ON_DEV = false;
 
-    public static final String RELEASE_VERSION = "Luffia/1.1.7.unstable-Release";
+    public static final String RELEASE_VERSION = "Luffia/1.1.8.unstable-Release";
 
     public static final String LICENSE = "GPL-3.0 license";
 
