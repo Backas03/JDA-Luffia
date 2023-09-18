6c65fc9e5c60c668f2372c30ff6af56b8ec292bf
wkgus0618 <wkgus0618@naver.com>
2023-09-19 00:46:10 +0900

    새로운 유틸 추가

--- src/main/java/kr/kro/backas/util/MemberUtil.java
+++ src/main/java/kr/kro/backas/util/MemberUtil.java
@@ -2,8 +2,11 @@ package kr.kro.backas.util;
 
 import kr.kro.backas.Main;
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.GuildVoiceState;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.entities.channel.concrete.VoiceChannel;
+import net.dv8tion.jda.api.entities.channel.unions.AudioChannelUnion;
 
 public final class MemberUtil {
     public static Member getMember(long userId) {
@@ -23,4 +26,20 @@ public final class MemberUtil {
                 String.format("%#s", user);
     }
 
+    public static boolean isInVoiceChannel(Member member) {
+        GuildVoiceState state = member.getVoiceState();
+        return state != null && state.inAudioChannel();
+    }
+
+    public static AudioChannelUnion getJoinedAudioChannel(Member member) {
+        GuildVoiceState state = member.getVoiceState();
+        if (state == null || !state.inAudioChannel()) return null;
+        return state.getChannel();
+    }
+
+    public static VoiceChannel getJoinedVoiceChannel(Member member) {
+        GuildVoiceState state = member.getVoiceState();
+        if (state == null || !state.inAudioChannel() || state.getChannel() == null) return null;
+        return state.getChannel().asVoiceChannel();
+    }
 }
