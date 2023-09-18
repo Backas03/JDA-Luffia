dc96dcf297aa67fa402c9318154b26b05763e1a8
wkgus0618 <wkgus0618@naver.com>
2023-09-19 00:44:37 +0900

    Legacy music player 코드 제거

--- src/main/java/kr/kro/backas/music/TrackUserData.java
+++
@@ -1,8 +0,0 @@
-package kr.kro.backas.music;
-
-import com.sedmelluq.discord.lavaplayer.player.AudioLoadResultHandler;
-import net.dv8tion.jda.api.entities.Message;
-
-public record TrackUserData(Message message) {
-
-}
