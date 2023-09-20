00be2bf1e845b7b57b2ef879f5dbeed39a47d80c
wkgus0618 <wkgus0618@naver.com>
2023-09-20 23:07:56 +0900

    Change accessor of extractVideoId to public

--- src/main/java/kr/kro/backas/music/service/youtube/YoutubeService.java
+++ src/main/java/kr/kro/backas/music/service/youtube/YoutubeService.java
@@ -7,7 +7,7 @@ public class YoutubeService {
         return "https://img.youtube.com/vi/" + videoId + "/0.jpg";
     }
 
-    private static String extractVideoId(String url) {
+    public static String extractVideoId(String url) {
         String videoId = null;
         String pattern = "(?<=watch\\?v=|/videos/|embed\\/|youtu.be\\/|\\/v\\/|\\/e\\/|watch\\?v%3D|watch\\?feature=player_embedded&v=|%2Fvideos%2F|embed%\u200C\u200B2F|youtu.be%2F|\\/v%2F)[^#\\&\\?\\n]*";
         java.util.regex.Pattern compiledPattern = java.util.regex.Pattern.compile(pattern);
