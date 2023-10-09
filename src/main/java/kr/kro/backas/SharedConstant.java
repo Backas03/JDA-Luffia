ba6f195f07064f8d6bd729b23f2fd2244c2e5e48
wkgus0618 <wkgus0618@naver.com>
2023-10-09 22:22:10 +0900

    oops

--- src/main/java/kr/kro/backas/SharedConstant.java
+++ src/main/java/kr/kro/backas/SharedConstant.java
@@ -5,7 +5,7 @@ public final class SharedConstant {
     public static final long DEV_GUILD_ID = 1121632283154202694L;
     public static final long PUBLISHED_GUILD_ID = MAIN_GUILD_ID;
 
-    public static final boolean ON_DEV = true;
+    public static final boolean ON_DEV = false;
 
     public static final String RELEASE_VERSION = "Luffia/2.0.4-Stable-Release";
 
