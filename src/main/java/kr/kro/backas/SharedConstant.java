24ed798e135ae86c0cd3c289ca1fb04c160bc3f9
Backas03 <wkgus0618@naver.com>
2023-12-31 21:37:24 +0900

    Version UP to 2.2.0-Stable-Release

--- src/main/java/kr/kro/backas/SharedConstant.java
+++ src/main/java/kr/kro/backas/SharedConstant.java
@@ -1,13 +1,13 @@
 package kr.kro.backas;
 
 public final class SharedConstant {
-    public static final long MAIN_GUILD_ID = 791974345965961237L;
-    public static final long DEV_GUILD_ID = 1121632283154202694L;
+    public static final long MAIN_GUILD_ID = 293977417482108930L;
+    public static final long DEV_GUILD_ID = 293977417482108930L;
     public static final long PUBLISHED_GUILD_ID = MAIN_GUILD_ID;
 
     public static final boolean ON_DEV = false;
 
-    public static final String RELEASE_VERSION = "Luffia/2.1.0-Stable-Release";
+    public static final String RELEASE_VERSION = "Luffia/2.2.0-Stable-Release";
 
     public static final String LICENSE = "MIT license";
 
