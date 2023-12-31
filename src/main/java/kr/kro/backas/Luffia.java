b71c798276a4993f976d58d785e628ed37002d20
Backas03 <wkgus0618@naver.com>
2023-12-31 21:32:51 +0900

    Replace chat commands with SlashCommands

--- src/main/java/kr/kro/backas/Luffia.java
+++ src/main/java/kr/kro/backas/Luffia.java
@@ -2,18 +2,12 @@ package kr.kro.backas;
 
 import kr.kro.backas.certification.CertificationManager;
 import kr.kro.backas.certification.listener.CertificationListener;
-import kr.kro.backas.command.*;
 import kr.kro.backas.command.api.CommandManager;
-import kr.kro.backas.command.certification.CertificationInfoCommand;
-import kr.kro.backas.command.certification.CertificationRemoveCommand;
-import kr.kro.backas.command.certification.ForceCertificationCommand;
-import kr.kro.backas.command.lol.LOLUserInfoCommand;
+import kr.kro.backas.command.lol.slash.LOLUserInfoSlashCommand;
 import kr.kro.backas.command.maplestory.MapleUserInfoCommand;
-import kr.kro.backas.command.music.*;
-import kr.kro.backas.command.slash.SlashCertificationCommand;
+import kr.kro.backas.command.music.slash.*;
 import kr.kro.backas.music.MusicListener;
 import kr.kro.backas.music.MusicPlayerController;
-import kr.kro.backas.secret.BotSecret;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.Guild;
@@ -32,34 +26,34 @@ public class Luffia {
 
         this.commandManager = new CommandManager("!", this);
 
-        this.commandManager.registerSlashCommand(new SlashCertificationCommand());
+        //this.commandManager.registerSlashCommand(new SlashCertificationCommand());
+        this.commandManager.registerSlashCommand(new PlaySlashCommand());
+        this.commandManager.registerSlashCommand(new QueueSlashCommand());
+        this.commandManager.registerSlashCommand(new QuitSlashCommand());
+        this.commandManager.registerSlashCommand(new SetRepeatModeSlashCommand());
+        this.commandManager.registerSlashCommand(new PauseOrResumeSlashCommand());
+        this.commandManager.registerSlashCommand(new LOLUserInfoSlashCommand());
 
-        this.commandManager.registerCommand("인증정보", new CertificationInfoCommand());
-        this.commandManager.registerCommand("인증해제", new CertificationRemoveCommand());
-        this.commandManager.registerCommand("도움말", new HelpCommand());
-        this.commandManager.registerCommand("강제인증", new ForceCertificationCommand());
+        //this.commandManager.registerCommand("인증정보", new CertificationInfoCommand());
+        //this.commandManager.registerCommand("인증해제", new CertificationRemoveCommand());
+        //this.commandManager.registerCommand("도움말", new HelpCommand());
+        //this.commandManager.registerCommand("강제인증", new ForceCertificationCommand());
 
-        this.commandManager.registerCommand("재생", new PlayCommand());
-        this.commandManager.registerCommand("나가기", new QuitCommand());
-        this.commandManager.registerCommand("스킵", new SkipCommand());
-        this.commandManager.registerCommand("일시정지", new PauseCommand());
-        this.commandManager.registerCommand("일시정지해제", new ResumeCommand());
-        //this.commandManager.registerCommand("전체반복", new RepeatAllCommand());
-        //this.commandManager.registerCommand("반복", new RepeatCurrentCommand());
-        //this.commandManager.registerCommand("반복해제", new NoRepeatCommand());
-        this.commandManager.registerCommand("대기열", new QueueCommand());
+        //this.commandManager.registerCommand("재생", new PlayCommand());
+        //this.commandManager.registerCommand("나가기", new QuitCommand());
+        //this.commandManager.registerCommand("스킵", new SkipCommand());
+        //this.commandManager.registerCommand("일시정지", new PauseCommand());
+        //this.commandManager.registerCommand("일시정지해제", new ResumeCommand());
 
-        this.commandManager.registerCommand("롤정보", new LOLUserInfoCommand());
+        //this.commandManager.registerCommand("롤정보", new LOLUserInfoCommand());
 
         this.commandManager.registerCommand("메이플정보", new MapleUserInfoCommand());
 
-        this.certificationManager = new CertificationManager(discordAPI);
+        this.certificationManager = null; // new CertificationManager(discordAPI);
 
         this.musicPlayerController = new MusicPlayerController();
+        this.musicPlayerController.register(discordAPI);
         discordAPI.addEventListener(this.musicPlayerController);
-        for (String botToken : BotSecret.MUSIC_BOT_TOKENS) {
-            this.musicPlayerController.register(botToken);
-        }
 
 
         this.discordAPI.addEventListener(new MusicListener());
