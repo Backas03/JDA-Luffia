5ec0a40487e59b3669abefee57f18af493e1e21e
Backas03 <71801733+Backas03@users.noreply.github.com>
2024-02-12 11:40:30 +0900

    Version Up

--- src/main/java/kr/kro/backas/SharedConstant.java
+++ src/main/java/kr/kro/backas/SharedConstant.java
@@ -7,7 +7,7 @@ public final class SharedConstant {
 
     public static final boolean ON_DEV = false;
 
-    public static final String RELEASE_VERSION = "Luffia/2.2.0-Stable-Release";
+    public static final String RELEASE_VERSION = "Luffia/2.2.1-Stable-Release";
 
     public static final String LICENSE = "MIT license";
 
