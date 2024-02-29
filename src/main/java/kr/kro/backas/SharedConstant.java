7198a5792728be4bd71db3b50e4f05c4df840ffe
wkgus <wkgus0618@naver.com>
2024-02-29 16:11:34 +0900

    디버그 메서드 제거

--- src/main/java/kr/kro/backas/SharedConstant.java
+++ src/main/java/kr/kro/backas/SharedConstant.java
@@ -1,15 +1,15 @@
 package kr.kro.backas;
 
 public final class SharedConstant {
+    public static final long EMAIL_VERIFICATION_ROLE_ID = -1L;
     public static final long MAIN_GUILD_ID = 1173281289470758913L;
     public static final long DEV_GUILD_ID = 1173281289470758913L;
     public static final long PUBLISHED_GUILD_ID = MAIN_GUILD_ID;
-
-    public static final String ZONE_ID = "Asia/Seoul";
+    public static final String DEFAULT_ACTIVITY = "/도움말 또는 !도움말  명령어로 기능 확인";
 
     public static final boolean ON_DEV = false;
 
-    public static final String RELEASE_VERSION = "Luffia/2.2.4-Stable-Release";
+    public static final String RELEASE_VERSION = "Luffia/2.3.0-Stable-Release";
 
     public static final String LICENSE = "MIT license";
 
